08dd8142b372baf523728ef428f8b1bb836a771e
b06502002 <b06502002@ntu.edu.tw>
2021-09-06 14:29:18 +0800

    Automated sync:  M readCSV.py ?? re1.csv

--- readCSV.py
+++ readCSV.py
@@ -50,7 +50,7 @@ def bdcoord(img):
 
 lst = pd.read_csv(csvPath)
 
-for ii in range(0,126,1):
+for ii in range(0,126,83):
     imgPath = basePath + lst.Deftype[ii] +'/'+ lst.Chip_ID[ii] +'/'
     if lst.Deftype[ii] == 'WSL128':
         for file_name in os.listdir(imgPath):
@@ -180,5 +180,3 @@ for ii in range(0,126,1):
     Contrast = abs(img1[cY,cX]-I_Back)/(img1[cY,cX]+I_Back)
     with open("re1.csv", 'a') as f:
         f.write(str(Contrast)+", "+str(lst.RealJND[ii])+"\n")
-    
-    # this is a comment for git test
