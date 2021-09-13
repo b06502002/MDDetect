489966564af403db421b4ea5a500f74f3cbd68c2
b06502002 <b06502002@ntu.edu.tw>
2021-09-13 15:58:10 +0800

    Automated sync:  M 2x2roi_JND.py  M re1.csv ?? img/ ?? saveNsortImg.py

--- saveNsortImg.py
+++ saveNsortImg.py
@@ -5,13 +5,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tifffile as tiff
 from matplotlib.pyplot import figure
-
+from skimage.util import img_as_ubyte
 # This is description
-    # calculate contrast
-    # based on the center of mass of extracted defects
+    # sort same images with same JND in the same file
 
 basePath = "/home/cov/Desktop/PML/project1_Mura/AUO_Data/2nd/0826_2nd/"
-csvPath = basePath + "table2.csv"
+csvPath = basePath + "table4.csv"
 
 
 def bdcoord(img):
@@ -54,7 +53,7 @@ def bdcoord(img):
 
 lst = pd.read_csv(csvPath)
 
-for ii in range(0,126,1):
+for ii in range(0,12,1):
     imgPath = basePath + lst.Deftype[ii] +'/'+ lst.Chip_ID[ii] +'/'
     if lst.Deftype[ii] == 'WSL128':
         for file_name in os.listdir(imgPath):
@@ -113,8 +112,10 @@ for ii in range(0,126,1):
         # img1 is .tif
         # img2 is .bmp
         # we use img2 to find the approximate location of defect
+    
     print("===============")
     print(lst.Chip_ID[ii])
+    print(lst.RealJND[ii])
     print("Img #" + str(ii))
     x_min, x_max, y_min, y_max = bdcoord(img1)
     img4 = img2[y_min//2:y_max//2,x_min//2:x_max//2]
@@ -125,89 +126,24 @@ for ii in range(0,126,1):
 
     img1 = img1[y_min:y_max,x_min:x_max]
     
-    # DCT
-    imgFloat = img1.astype('float')
-    coeff = cv2.dct(imgFloat)
-    coeff[:,3:]=0
-    coeff[3:][:]=0
-    reconsImg = cv2.idct(coeff)
-    diff = reconsImg-img1
-
-
     y_bd = min(2*a[0]+224,y_max)
     x_bd = min(2*a[1]+224,x_max)
     img1 = img1[2*a[0]:y_bd,2*a[1]:x_bd]
     # plt.imshow(img1)
     # plt.show()
-    diff = diff[2*a[0]:y_bd,2*a[1]:x_bd]
-    # plt.imshow(diff)
-    # plt.show()
-
-    diff_n = cv2.normalize(diff, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_8UC1)
-    thresh1 = cv2.adaptiveThreshold(diff_n, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 45, 22)# obtain white Mura
-    thresh2 = cv2.adaptiveThreshold(diff_n, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 45,-22)# obtain black Mura
-    BothT = thresh1+thresh2
-    BothT2 = cv2.medianBlur(BothT, 9)
-    kernel = np.ones((2,2),np.uint8)
-    opening = cv2.morphologyEx(BothT2, cv2.MORPH_OPEN, kernel)
     
-    # plt.imshow(opening)
-    # plt.show()
-
-    color = (255,200,200)#(200,200,200)
-    lst_dfcts = []
-    lst_bdbox = []
-
-    _, opening2 = cv2.threshold(opening, 127, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(opening2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    directory = str(lst.RealJND[ii]).replace(".",'_')
+    parent_dir = "./img/"
+    pathtoimg = os.path.join(parent_dir, directory)
     
-    flag = 0
-    if len(contours) == 0: # This if-else should be replaced by better CV algorithms in later versions
-        continue
-    else:
-        for i in reversed(range(len(contours))):
-            cimg,cimg2 = np.zeros_like(opening), np.zeros_like(opening)
-            cv2.drawContours(cimg, contours, i, color, -1, cv2.LINE_8)
-            pts = np.where(cimg == 255)
-            lst_dfcts.append(img1[pts[0], pts[1]]) # record the intensity at the defect pixels
-            bdRect = cv2.boundingRect(contours[i])
-            cv2.rectangle(cimg2, (int(bdRect[0]), int(bdRect[1])),\
-                        (int(bdRect[0]+bdRect[2]), int(bdRect[1]+bdRect[3])), 255, -1)
-            pts2 = np.where(cimg2 == 255)
-            lst_bdbox.append(img1[pts2[0], pts2[1]])
+    if not os.path.exists(pathtoimg):
+        os.mkdir(pathtoimg)
+    
 
-            M = cv2.moments(contours[i])
-            
-            if M["m00"]!=0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                # if flag == 0 and abs(cY-57)<10:
-                #     print(cX,cY)
-                if abs(cY-57)<15 and abs(cX-55)<15: # "and" is NOT same as &
-                    print(cX,cY)
-                    break
-            else:
-                cX,cY = 0,0
-            
-    total_pixel_count = img1.shape[0]*img1.shape[1]
-    defect_pixel_count = 0
-    for i in range(len(lst_dfcts)):
-        defect_pixel_count += len(lst_dfcts[i])
+    img8bit = img_as_ubyte(img1)
+    cv2.imwrite(pathtoimg+"/"+str(lst.Chip_ID[ii])+".png", img8bit)
 
-    defect_intens = 0
-    for i in range(len(lst_dfcts)):
-        defect_intens += sum(lst_dfcts[i])
-    total_intens = cv2.sumElems(img1)[0] - defect_intens
-
-    I_Back = total_intens/(total_pixel_count-defect_pixel_count)
-    roisum = cv2.sumElems(img1[cY:cY+1,cX:cX+1])
-    I_new = roisum[0]*255/(4*abs(roisum[0]/4-I_Back))
-    IB_new = I_Back*255/(abs(roisum[0]/4-I_Back))
-    Contrast = abs(I_new-IB_new)/(I_new+IB_new)
-
-    # I_Back = total_intens/(total_pixel_count-defect_pixel_count)
-    # Contrast = abs(img1[cY,cX]-I_Back)/(img1[cY,cX]+I_Back)
-    with open("re1.csv", 'a') as f:
-        if ii == 0:
-            f.write("Pred, Real\n")
-        f.write(str(Contrast)+", "+str(lst.RealJND[ii])+"\n")
+    # with open("re1.csv", 'a') as f:
+    #     if ii == 0:
+    #         f.write("Pred, Real\n")
+    #     f.write(str(Contrast)+", "+str(lst.RealJND[ii])+"\n")
