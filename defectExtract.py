97b519cb7aa1ece2f17fedaeee0dd9d3a472ad7e
b06502002 <b06502002@ntu.edu.tw>
2021-10-01 11:39:42 +0800

    Automated sync:  M defectExtract.py

--- defectExtract.py
+++ defectExtract.py
@@ -3,36 +3,48 @@ import cv2
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from tifffile.tifffile import astype
 
 def main(pathh):
+    ddepth = cv2.CV_64F
     csvPath = pathh + "filenames.csv"
     lst = pd.read_csv(csvPath)
-    # ct = 0
-    # for fname in os.listdir(pathh):
-    #     img1 = cv2.imread(pathh + fname)
-    #     print(fname)
-    #     imgFloat = img1.astype('uint8')
-    #     print(imgFloat.max())
-    #     plt.imshow(imgFloat)
-    #     plt.show()
-    #     ct += 1
-    #     if ct == 3:
-    #         break
     imgLst = []
-    for ii in range(3):
+    flagg = 0
+    st = 288
+    ed = 309
+    for ii in range(st,ed):
         imgLst.append(cv2.imread(pathh+lst.fname[ii], cv2.IMREAD_GRAYSCALE))
-        if ii == 0:
+        if flagg == 0:
             Bimg = np.zeros_like(imgLst[0], dtype='float64')
-        Bimg += imgLst[ii].astype('float64')
+            flagg = 1
+        Bimg += imgLst[ii-st].astype('float64')
 
-    Bimg //= 3
+    Bimg //= (ed-st)
     plt.imshow(Bimg,cmap='gray')
     plt.show()
 
-    for ii in range(3):
+    for ii in range(ed-st):
+        diff = imgLst[ii]-Bimg
+        plt.imshow(diff,cmap='gray')
+        plt.show()
+        # grad_x = cv2.Sobel(imgLst[ii], ddepth, 1, 0, ksize=5, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+        # grad_y = cv2.Sobel(imgLst[ii], ddepth, 0, 1, ksize=5, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+        # abs_grad_x = cv2.convertScaleAbs(grad_x)
+        # abs_grad_y = cv2.convertScaleAbs(grad_y)
+        # grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+        # plt.imshow(grad)
+        # plt.show()
 
-        plt.imshow(imgLst[ii]-Bimg,cmap='gray')
+        diff_n = cv2.normalize(diff, None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_8UC1)
+        cv2.imwrite('/home/cov/Desktop/PML/progress report/2021 Oct 1/'+'{}.jpg'.format(ii), diff_n)
+        thresh1 = cv2.adaptiveThreshold(diff_n, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 51, 25)# obtain white Mura
+        thresh2 = cv2.adaptiveThreshold(diff_n, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 51,-25)# obtain black Mura
+        BothT = thresh1+thresh2
+        BothT2 = cv2.medianBlur(BothT, 7)
+        kernel = np.ones((2,2),np.uint8)
+        opening = cv2.morphologyEx(BothT2, cv2.MORPH_OPEN, kernel)
+        cv2.imwrite('/home/cov/Desktop/PML/progress report/2021 Oct 1/'+'{}t.jpg'.format(ii), opening)
+        plt.imshow(opening,cmap='gray')
         plt.show()
 
     return 0
